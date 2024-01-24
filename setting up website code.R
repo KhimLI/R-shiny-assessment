00d8d2802d086d9ee45a553ed8a1b0b16552fe41
Wan Khim Li <khimli.wan@n-actuarial.com>
2024-01-24 20:57:18 +0800

    Allow user to input excel file, and then output the table.

--- setting up website code.R
+++ setting up website code.R
@@ -1,11 +1,28 @@
 library(shiny)
+library(readxl)
 
 ui <- fluidPage(
   
-)
-
-server <- function(Input,Output,Session){
+  titlePanel("Claim Data Upload"),
   
+  sidebarLayout(
+    sidebarPanel(
+      fileInput(
+        inputId = "file",
+        label = "Upload your file",
+        accept = ".xlsx"
+        )
+      ),
+    mainPanel(
+      tableOutput("content")
+      )
+    )
+)
+server <- function(input,output,session){
+  output$content <- renderTable({
+    req(input$file)
+    read_excel(input$file$datapath)
+  })
 }
 
 shinyApp(ui,server)
